97499f2fcd7a06882be25b4b77b7d6ee0ef1d076
bauferllor <bauferllor@gmail.com>
2024-11-18 18:01:00 -0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,11 +1,4 @@
-from Juego_oca import jugar as jugar_oca
-from adivina_capitales import jugar as jugar_capitales
-from adivinaydescifra import jugar as jugar_descifra
-from buscaminas import jugar as jugar_buscaminas
-from carrera_mente import jugar as jugar_mente
-
-def menu():
-    while True:
+def mostrar_menu():
         print("\n--- Menú Principal ---")
         print("1. Jugar al Juego de la Oca")
         print("2. Jugar a Adivina las Capitales")
@@ -14,28 +7,36 @@ def menu():
         print("5. Jugar a Carrera de Mente")
         print("6. Salir")
         
-        opcion = input("Selecciona una opción: ")
+
+while True:
+    mostrar_menu()
+    opcion = input("Selecciona una opción: ")
         
-        if opcion == "1":
-            print("\n¡Iniciando el Juego de la Oca!")
-            jugar_oca()
-        elif opcion == "2":
-            print("\n¡Iniciando Adivina las Capitales!")
-            jugar_capitales()
-        elif opcion == "3":
-            print("\n¡Iniciando Adivina y Descifra!")
-            jugar_descifra()
-        elif opcion == "4":
-            print("\n¡Iniciando el Buscaminas!")
-            jugar_buscaminas()
-        elif opcion == "5":
-            print("\n¡Iniciando Carrera de Mente!")
-            jugar_mente()
-        elif opcion == "6":
-            print("\n¡Gracias por jugar! Hasta la próxima.")
-            break
-        else:
-            print("\nOpción no válida. Intenta de nuevo.")
+    if opcion == "1":
+        print("\n¡Iniciando el Juego de la Oca!")
+        import Juego_oca 
+        Juego_oca.jugar()
+    elif opcion == "2":
+        print("\n¡Iniciando Adivina las Capitales!")
+        import adivina_capitales 
+        adivina_capitales.juego_adivinar_capital()
+    elif opcion == "3":
+        print("\n¡Iniciando Adivina y Descifra!")
+        import adivinaydescifra 
+        adivinaydescifra.jugar_adivinaydescifra()
+    elif opcion == "4":
+        print("\n¡Iniciando el Buscaminas!")
+        import buscaminas 
+        buscaminas.jugar_buscamina()
+    elif opcion == "5":
+        print("\n¡Iniciando Carrera de Mente!")
+        import carrera_mente  
+        carrera_mente.juego_carrera_de_mente()
+    elif opcion == "6":
+        print("\n¡Gracias por jugar! Hasta la próxima.")
+        break
+    else:
+        print("\nOpción no válida. Intenta de nuevo.")
 
 if __name__ == "__main__":
-    menu()
+    mostrar_menu()
